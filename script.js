6d538a2c04d635fb6f553d88890c063018a5c341
bobur <boburmakhmadkulov05@gmail.com>
2023-09-28 20:04:56 +0500

    console changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22];
 
 let string = arr.filter((str) => typeof str === "string");
-console.log(string);
+console.log(string.length);
 
 let number = arr.filter((num) => typeof num === "number");
-console.log(number);
+console.log(number.length);
 
 let others = arr.filter((oth) => typeof oth !== "string" && typeof oth !== "number");
-console.log(others);
+console.log(others.length);
